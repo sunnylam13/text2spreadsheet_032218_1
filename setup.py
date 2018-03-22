5911311d21435db1a81a098232f0a28a83918527
Sunny Lam <sunny.lam@gmail.com>
2018-03-22 12:44:54 -0400

    revised setup

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ except ImportError:
 config = {
 	'description': 'This program reads several text files and sticks the content into a spreadsheet, one line per row.  Each text file gets a column of its own.',
 	'author': 'Sunny Lam',
-	'url': 'URL to get it at',
-	'download_url': 'Where to download it',
-	'author_email': 'My email',
+	'url': 'https://github.com/sunnylam13/text2spreadsheet_032218_1',
+	'download_url': 'https://github.com/sunnylam13/text2spreadsheet_032218_1',
+	'author_email': 'sunny.lam@gmail.com',
 	'version': '0.1',
 	'install_requires': ['nose'],
 	'packages': ['openpyxl'],
